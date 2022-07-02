a9f930ba8e32e34b6a0ccb62e7fcb1500ace9eb1
endigo9740 <gundamx9740@gmail.com>
2022-07-02 15:05:18 -0500

    Added SvelteKit static adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -12,7 +13,18 @@ const config = {
 	],
 
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: 'index.html',
+			precompress: false
+		}),
+		prerender: {
+			// This can be false if you're using a fallback (i.e. SPA mode)
+			default: false
+		},
 		alias: {
 			$lib: 'src/lib',
 			$util: 'src/util',
