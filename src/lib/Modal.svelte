7e3842d5b4939e3b2a6b65a730f0f5718f1dc27f
endigo9740 <gundamx9740@gmail.com>
2022-07-03 12:58:04 -0500

    Work details view component added

--- src/lib/Modal.svelte
+++ src/lib/Modal.svelte
@@ -1,14 +1,16 @@
 <script lang="ts">
     import Works from "$routes/works/index.svelte";
     import About from "$routes/about.svelte";
+    import Details from "$routes/works/[category]/Details.svelte";
 </script>
 
 <section class="fixed top-0 left-0 right-0 bottom-0 z-40 flex justify-center items-center">
     
     <!-- Window -->
-    <div class="w-[90%] max-h-[75%] bg-slate-900/[0.98] border border-white/20 rounded-xl shadow-2xl m-10 overflow-x-auto">
+    <div class="w-[90%] max-h-[75%] bg-slate-900/[0.98] border border-white/20 rounded-xl shadow-2xl m-10 overflow-y-auto">
         <!-- <Works /> -->
-        <About compact={true} />
+        <!-- <About compact={true} /> -->
+        <Details category='brain-and-bones' id="skeleton" />
     </div>
 
 </section>
---
+++ src/routes/works/[category]/Details.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+    import { works } from '$lib/works';
+
+	export let category: string;
+	export let id: string;
+
+    const categoryLabel = works[category].label;
+    const work: any = works[category].projects.find((p: any) => p.id === id);
+</script>
+
+<!-- Page -->
+{#if works}
+<main class="page flex">
+    
+    <!-- Info -->
+    <section class="w-[40%] h-full p-4 space-y-10 overflow-y-auto">
+        <a href="/works">&larr; Back to Works</a>
+        <header class="space-y-2">
+            <h2 class="text-sm text-white/70 uppercase">{categoryLabel}</h2>
+            <h1 class="section-label text-4xl">{work.name}</h1>
+        </header>
+        <a href={work.url} class="inline-block btn btn-hollow" target="_blank">Launch</a>
+        <div>{@html work.details}</div>
+    </section>
+
+    <!-- Media -->
+    <section class="w-[60%] h-full bg-black/20 border-l border-white/10 py-4 px-20 backdrop-blur-xl flex flex-col items-center space-y-10 overflow-y-auto">
+        {#if work.screenshots.length === 0}<p>No screenshots available.</p>{/if}
+        {#each work.screenshots as screenshot}
+            <img src={screenshot} class="shadow-xl rounded-xl" alt="example">
+        {/each}
+    </section>
+
+</main>
+{/if}
