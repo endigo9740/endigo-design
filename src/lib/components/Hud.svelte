d988739591b692a4632bd2d0a0a9a1b7aa9dc254
endigo9740 <gundamx9740@gmail.com>
2022-07-08 16:49:07 -0500

    Added basic music player

--- src/lib/components/Hud.svelte
+++ src/lib/components/Hud.svelte
@@ -1,13 +1,35 @@
 <script lang="ts">
+import { onMount } from 'svelte';
+
     import { fly } from 'svelte/transition';
 
     export let camera: any;
     export let pillarList: any;
     export let npcList: any;
 
+    // Camera Center on Target
     let selectedTarget: any;
-    function centerOnContainer(): void { camera.centerOnContainer(selectedTarget); }
+    function centerOnContainer(): void {
+        camera.centerOnContainer(selectedTarget);
+    }
+
+    // Music Player
+    let elemAudio: HTMLAudioElement;
+    let musicPlaying: boolean = false;
+    onMount(() => {
+        elemAudio.pause();
+        elemAudio.volume = 0.5;
+    });
+    function onToggleMusic(): void {
+        elemAudio.paused === true ? elemAudio.play() : elemAudio.pause();
+        musicPlaying = !elemAudio.paused;
+    }
 </script>
+
+<!-- Music -->
+<audio bind:this={elemAudio} hidden autoplay loop preload="auto" class="fixed left-4 bottom-4 z-20">
+    <source src="http://cdn.endigodesign.com.s3-us-east-2.amazonaws.com/endigo_design/portfolio/music.mp3" type="audio/mpeg">
+</audio>
 
 <!-- Branding -->
 <a
@@ -23,29 +45,33 @@
     </div>
 </a>
 
-<!-- Navigation -->
+<!-- Controls -->
 <nav class="fixed bottom-0 right-0 z-50 bg-slate-900/90 p-6 rounded-tl-xl space-x-4 backdrop-blur" transition:fly|local={{y: 100, duration: 250}}>
-    <label for="selection">
 
-        {#if pillarList}
-        <select name="selection" id="selection" class="max-w-[120px] text-center" bind:value={selectedTarget} on:change={() => {centerOnContainer()}}>
-            <option disabled>Navigate</option>
-            <optgroup label="Pillars">
-                {#each pillarList as pillar, i}
-                    <!-- {#if pillar.found} -->
-                    <option value={pillar}>{pillar.page.category ? `${pillar.page.category} /` : ''} {pillar.name}</option>
-                    <!-- {:else}
-                    <option value={pillar}>Unactivated Pillar #{i+1}</option>
-                    {/if} -->
-                {/each}
-            </optgroup>
-            <optgroup label="Characters">
-                {#each npcList as npc}
-                <option value={npc}>{npc.name}</option>
-                {/each}
-            </optgroup>
-        </select>
-        {/if}
+    <!-- Navigation -->
+    {#if pillarList}
+    <select name="selection" id="selection" class="max-w-[120px] text-center" bind:value={selectedTarget} on:change={() => {centerOnContainer()}}>
+        <option disabled>Navigate</option>
+        <optgroup label="Pillars">
+            {#each pillarList as pillar, i}
+                <!-- {#if pillar.found} -->
+                <option value={pillar}>{pillar.page.category ? `${pillar.page.category} /` : ''} {pillar.name}</option>
+                <!-- {:else}
+                <option value={pillar}>Unactivated Pillar #{i+1}</option>
+                {/if} -->
+            {/each}
+        </optgroup>
+        <optgroup label="Characters">
+            {#each npcList as npc}
+            <option value={npc}>{npc.name}</option>
+            {/each}
+        </optgroup>
+    </select>
+    {/if}
 
-    </label>
+    <!-- Music Toggle -->
+    <button type="button" class="btn-hollow" on:click={() => { onToggleMusic(); }}>
+        <div class:animFloatSmall={musicPlaying}>♫</div>
+    </button>
+
 </nav>
