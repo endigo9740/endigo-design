acb57ea99e67982a93dbb84953d42516b3fc798b
endigo9740 <gundamx9740@gmail.com>
2022-07-12 17:10:07 -0500

    Layla and dialog adjusted

--- src/lib/components/Dialog.svelte
+++ src/lib/components/Dialog.svelte
@@ -6,20 +6,21 @@
 </script>
 
 {#if $dialogStore !== undefined}
-<section class="bg-glass window fixed left-4 right-4 bottom-10 z-20 max-w-[1200px] mx-auto p-6 flex items-start space-x-8" transition:fly={{y: 500, duration: 150}}>
+<section class="bg-glass window fixed left-4 right-4 bottom-10 z-20 max-w-[1200px] mx-auto p-6 flex items-center space-x-8" transition:fly={{y: 500, duration: 150}}>
 
     <!-- Portrait -->
     <img src={$dialogStore.portrait} class="flex-none image-crisp w-16 md:w-[160px] -mt-14 aspect-square border border-white rounded-xl shadow-xl" alt="portrait">
 
     <!-- Text -->
-    <div class="flex-auto space-y-6">
+    <div class="flex-auto space-y-2">
         <h3>{$dialogStore.name}</h3>
         <p class="text-xl">{@html $dialogStore.message}</p>
-        <!-- Actions -->
-        <nav>
-            <button type="button" class="btn-hollow" on:click={dismiss}>Okay, got it!</button>
-        </nav>
     </div>
+
+    <!-- Actions -->
+    <nav class="space-y-4">
+        <button type="button" class="btn-hollow" on:click={dismiss}>Okay, got it!</button>
+    </nav>
     
 </section>
 {/if}
