be2c50be8381b1b1a3543c593f02f35c62fd65ef
endigo9740 <gundamx9740@gmail.com>
2022-07-14 10:47:52 -0500

    Hud adjustments

--- src/lib/components/Hud.svelte
+++ src/lib/components/Hud.svelte
@@ -21,7 +21,7 @@
 <!-- Branding -->
 <a
     href="/"
-    class="fixed top-0 left-0 z-10 bg-slate-900/90 p-6 rounded-br-xl flex items-center space-x-4 backdrop-blur hover:bg-slate-800/90"
+    class="fixed top-0 left-0 z-10 bg-slate-900/50 p-6 rounded-br-xl flex items-center space-x-4 backdrop-blur hover:bg-slate-800/50"
     transition:fly|local={{y: -100, duration: 250}}
     title="Go to homepage."
 >
@@ -33,7 +33,7 @@
 </a>
 
 <!-- Controls -->
-<nav class="fixed bottom-0 right-0 z-50 bg-slate-900/90 p-6 rounded-tl-xl backdrop-blur space-x-4" transition:fly|local={{y: 100, duration: 250}}>
+<nav class="fixed bottom-0 right-0 z-50 bg-slate-900/50 p-4 rounded-tl-xl backdrop-blur space-x-4" transition:fly|local={{y: 100, duration: 250}}>
 
     <!-- Menu: Navigate -->
     <button type="button" class="btn-hollow" on:click={() => { toggleMenu(); }}>Navigate</button>
