8e0f1f3a23e41fbc162763c315a661a8caf30d07
endigo9740 <gundamx9740@gmail.com>
2024-01-16 17:42:39 -0600

    Astro cloudflare adapter added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+
+  // Adapter: Cloudflare
+  output: 'server',
+  adapter: cloudflare(),
 });
