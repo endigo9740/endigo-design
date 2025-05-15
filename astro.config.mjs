a78af97e7a5f20d2c4f99e7f1604b20de5501c22
Chris Simmons <gundamx9740@gmail.com>
2025-05-15 12:14:37 -0500

    Version 3.0 (#14)
    * Migrate to latest dependencies
    
    * Integrate Skeleton, resolve style issues
    
    * Conver to Astro content collections and format MDX
    
    * Content updates
    
    * Content and image updates
    
    * Polish and glitch effects

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,32 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwind from "@astrojs/tailwind";
-import cloudflare from '@astrojs/cloudflare';
+import cloudflare from "@astrojs/cloudflare";
+import mdx from "@astrojs/mdx";
+import tailwindcss from "@tailwindcss/vite";
+import AutoImport from "astro-auto-import";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-
-  // Adapter: Cloudflare
-  output: 'server',
+  output: "server",
+  // https://docs.astro.build/en/guides/integrations-guide/cloudflare/
   adapter: cloudflare(),
+  integrations: [
+    // https://github.com/delucis/astro-auto-import/tree/main/packages/astro-auto-import
+    AutoImport({
+      imports: [
+        {
+          // import componentSet from "@components/mdx/index";
+          "@components/mdx/index": [["default", "componentSet"]],
+        },
+      ],
+    }),
+    // https://docs.astro.build/en/guides/integrations-guide/mdx/
+    mdx(),
+  ],
+  vite: {
+    plugins: [
+      // https://tailwindcss.com/docs/installation/framework-guides/astro
+      tailwindcss(),
+    ],
+  },
 });
